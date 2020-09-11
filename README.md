10ab91b03cff5400b93ba804f3f9d5a494807b1a
Chris C <christopher.cruzcosa@gmail.com>
2020-09-11 16:25:56 -0700

    Modified ReadMe and added notations to the javascript file

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # auto-password-generator
 A small tool to automatically generate passwords of specified complexity.
+
+
+## Installation
+
+This web app is made up of an Index.html file, a style.css file, and a script.js file, which controls the password generator functionality.
+
+The location is: https://christopher-cruzcosa.github.io/auto-password-generator/ 
+
+
+## Usage 
+
+![Screenshot](./assets/screenshot1.png)
+
+The user will initially see the password generater with placeholder text.  After hitting the "generate" button, the user will be present with a series of prompts that require the user to pick the following attributes of a password:
+
+    1) Length - the user will be given a 'prompt' asking them to type a numeral from 8 to 128, to set the password length; passing in a numeral out of bounds or a value that is not a numeral will send an alert warning the user and then return them to the original 'prompt' until they have entered a valid response
+
+    2) Attributes - the user will be told to select "yes/no" to four options, each with their own 'confirm', for which types of characters to make up the password; saying "no" to all four options will send an alert warning the user and then return them to the original series of 'confirms' until they have selected at least one of the options; the types of characters are:
+        i.   Lower case letters of the ENG alphabet
+        ii.  Upper case letters of the ENG alphabet
+        iii. Special characters
+        iV.  Numerals
+
+Once the attributes have been set, the user will be alerted that the new password has been generated, and the new password will appear in the browser window, replacing the placeholder text in the password generater box. 
+
+
+## Credits
+
+Created by: Christopher Cruzcosa
+
+
+
+## License
+
+Copyright (c) [2020] [Christopher Cruzcosa]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,43 @@
+
+// This variable makes a hotlink to the #generate button//
 var generateBtn = document.querySelector("#generate");
 
+// This overall function generates the new password, upong the button being clicked by the user//
 function generatePassword() {
+
+  // This variable takes in the user's response to a prompt asking for a password length in numerals//
   var lengthChoice = prompt("Criteria 1 of 2: Password Length\n\nPick a password length by typing a number from 8 to 128:");
 
+  // This while loop does not let the user proceed until they have given a valid response//
   while (isNaN(parseInt(lengthChoice)) === true || parseInt(lengthChoice) > 128 || parseInt(lengthChoice) < 8) {
     alert("Sorry, '" + lengthChoice + "' is not a valid choice.\n\nPlease select a number from 8 to 128, to set your password length.");
     lengthChoice = prompt("Criteria 1 of 2: Password Length\n\nPick a password length by typing a number from 8 to 128:");
   };
 
+  // This alert confirms and returns the users' password length//
   alert("Thank you" + "\n\n" + "The password will be " + lengthChoice + " characters long");
 
+  // This variable lets the function know if the user has picked at least one character set //
   var characterTypeCount = false;
+
+  // These variables let the function know if the user has picked each individual character set//
   var lowerCaseChar = false;
   var upperCaseChar = false;
   var specialChar = false;
   var numChar = false;
 
+  // These confirms ask the user to say yes or no to each character set in sequence, and then changes the above variables if "yes"//
   lowerCaseChar = confirm("Criteria 2 of 2: Character Type(s)\n\nNote that at least one of the following four types must be selected:\nLower Case, Upper Case, Special Characters, and/or Numerals\n\nDo you want to use 'Lower Case' characters?\n('Ok' = 'Yes' and 'Cancel' = 'No')");
   upperCaseChar = confirm("Criteria 2 of 2: Character Type(s)\n\nNote that at least one of the following four types must be selected:\nLower Case, Upper Case, Special Characters, and/or Numerals\n\nDo you want to use 'Upper Case' characters?\n('Ok' = 'Yes' and 'Cancel' = 'No')");
   specialChar = confirm("Criteria 2 of 2: Character Type(s)\n\nNote that at least one of the following four types must be selected:\nLower Case, Upper Case, Special Characters, and/or Numerals\n\nDo you want to use 'Special' characters?\n('Ok' = 'Yes' and 'Cancel' = 'No')");
   numChar = confirm("Criteria 2 of 2: Character Type(s)\n\nNote that at least one of the following four types must be selected:\nLower Case, Upper Case, Special Characters, and/or Numerals\n\nDo you want to use 'Numeral' characters?\n('Ok' = 'Yes' and 'Cancel' = 'No')");
 
-
+  // This if statement checks if any character set was selcted, if yes, then it changes characterTypeCount to true//
   if (lowerCaseChar === true || upperCaseChar === true || specialChar === true || numChar === true) {
     characterTypeCount = true
   };
-
+  
+  // This while loop does not let the user continue if they have not yet selected at least one character set; if not, it cycles back through the confirms and then rechecks if it needs to change the characterTypeCount //
   while (characterTypeCount === false) {
     alert("Sorry, you cannot move forward unti you select at least one\nof the following four types of characters:\n\nLower Case, Upper Case, Special Characters, and/or Numerals");
     lowerCaseChar = confirm("Criteria 2 of 2: Character Type(s)\n\nNote that at least one of the following four types must be selected:\nLower Case, Upper Case, Special Characters, and/or Numerals\n\nDo you want to use 'Lower Case' characters?\n('Ok' = 'Yes' and 'Cancel' = 'No')");
@@ -37,62 +49,47 @@ function generatePassword() {
     };
   }
 
-
-  // if (lowerCaseChar === true && upperCaseChar === true && specialChar === true) {
-  //   alert("Thank you\n\nYou have selected: Lower Case, Upper Case, and Special Characters\n\nYou must really not want anyone to get into your account, lolz")
-  // } else if (lowerCaseChar === true && upperCaseChar === false && specialChar === false) {
-  //   alert("Thank you\n\nYou have selected: Lower Case Characters only")
-  // } else if (lowerCaseChar === true && upperCaseChar === false && specialChar === true) {
-  //   alert("Thank you\n\nYou have selected: Lower Case, and Special Characters")
-  // } else if (lowerCaseChar === true && upperCaseChar === true && specialChar === false) {
-  //   alert("Thank you\n\nYou have selected: Lower Case and Upper Case Characters")
-  // } else if (lowerCaseChar === false && upperCaseChar === true && specialChar === false) {
-  //   alert("Thank you\n\nYou have selected: Upper Case Characters only")
-  // } else if (lowerCaseChar === false && upperCaseChar === true && specialChar === true) {
-  //   alert("Thank you\n\nYou have selected: Upper Case and Special Characters")
-  // } else if (lowerCaseChar === false && upperCaseChar === false && specialChar === true) {
-  //   alert("Thank you\n\nYou have selected: Special Characters only")
-  // };
-
-  // var lowerChoice = if(lowerCaseChar === true) {"Yes"} else {"No"}
-  // var upperChoice = if(upperCaseChar === true) {"Yes"} else {"No"}
-  // var specialChoice = if(specialCaseChar === true) {"Yes"} else {"No"}
-  // var numChoice = if(numCaseChar === true) {"Yes"} else {"No"}
-
+  // This alert confirms that the user has selceted at least one chracter count and returns all four choices //
   alert ("Thank you, you have chosen:\n\n" + "Lower Case Characters: " + lowerCaseChar + "\nUpper Case Characters: " + upperCaseChar + "\nSpecial Characters: " + specialChar + "\nNumeral Characters: " + numChar )
 
+  // These variables contain all possible characters, sorted by charcters set, each in a seperate single string //
   var charSetLower = "abcdefghijklmnopqrstuvwxyz";
   var charSetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var charSetSpecial = "!\#$%&'()*+,-./:;<=>?@[]^_{|}~";
   var charSetNum = "0123456789";
+
+  // This variable is the list of characters that the user selected, in selecting the four character sets; it begins as an empty string //
   var characterOptions = "";
 
+  // The series of ifs below, check if a user selected an individual character set and, if yes, adds those characters to the characterOptions string//
   if (lowerCaseChar === true) {
     characterOptions += charSetLower;
-    
   };
   if (upperCaseChar === true) {
     characterOptions += charSetUpper;
-    }
-  
+  };
   if (specialChar === true) {
     characterOptions += charSetSpecial;
   };
-
   if (numChar === true) {
     characterOptions += charSetNum;
   };
 
+  // This variable is the string representing the newly generated password, which is intially empty //
   var passwordEntry = "";
 
+  // This for loop runs as many times as the player selected as the password length, each time adding a randowm character from the newly populated characterOptions string //
   for (var i = 1; i < (parseInt(lengthChoice)+1); i++) {
     passwordEntry += characterOptions.charAt(Math.floor(Math.random() * (characterOptions.length-1)));
   };
 
+  // This returns the newly generated password for the writePassword function to take in //
   return passwordEntry;
 
 }
 
+// This function takes in the new passwordEntry string, finds the place on the window that contains #password and then overrides whatever text is already there with the new password, 
+//finally it pops up an alert to tell the user that a new password has been generated //
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -102,4 +99,5 @@ function writePassword() {
 
 }
 
+// This creates the listener event that causes the writePassword function to be called after the user clicks the "generate" button //
 generateBtn.addEventListener("click", writePassword);
